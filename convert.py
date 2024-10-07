d4bc7a4cd361f154f0c6a4b0c56b4cd155321f3d
astrokompas <129834159+astrokompas@users.noreply.github.com>
2024-10-07 12:09:45 +0200

    added including all files in directory instead of providing specific path

--- convert.py
+++ convert.py
@@ -10,11 +10,20 @@ def pdf_to_images(pdf_path, output_folder, image_format='png', dpi=150):
     for page_num in range(pdf_document.page_count):
         page = pdf_document.load_page(page_num)
         pix = page.get_pixmap(dpi=dpi)
-        image_filename = f"{output_folder}/page_{page_num + 1}.{image_format}"
+        pdf_filename = os.path.splitext(os.path.basename(pdf_path))[0]
+        image_filename = f"{output_folder}/{pdf_filename}_page_{page_num + 1}.{image_format}"
         pix.save(image_filename)
         print(f"Saved {image_filename}")
 
+def convert_all_pdfs_in_folder(input_folder, output_folder, image_format='png', dpi=150):
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith('.pdf'):
+            pdf_path = os.path.join(input_folder, filename)
+            print(f"Processing {pdf_path}")
+            pdf_to_images(pdf_path, output_folder, image_format, dpi)
+
 if __name__ == "__main__":
-    pdf_path = "CV.pdf"
+    input_folder = "."
     output_folder = "."
-    pdf_to_images(pdf_path, output_folder)
+	
+    convert_all_pdfs_in_folder(input_folder, output_folder)
